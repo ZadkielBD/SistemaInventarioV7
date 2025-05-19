f8e0be4a5404af024e38a5d922f54d526d63fb81
julio cesar barron <nirvan13@hotmail.com>
2025-05-19 14:51:13 -0600

    Crear Proyecto

---
+++ SistemaInventarioZ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventarioZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
