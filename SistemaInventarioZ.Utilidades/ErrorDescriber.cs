ed0061393100ff527dfbcb464d683653975389c0
Zadkiel Lupito tiktok <zadkielbd@outlook.com>
2025-06-04 15:16:33 -0600

    Administracion de Usuarios

---
+++ SistemaInventarioZ.Utilidades/ErrorDescriber.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioZ.Utilidades
+{
+    public class ErrorDescriber : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = nameof(PasswordRequiresLower),
+                Description = "El password require al menos de una letra minuscula"
+            };
+        }
+    }
+}
